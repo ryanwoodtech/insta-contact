496c9daabfce32be0cb89a735f3ed6007682154f
Ryan Wood <ryan@ryanwood.tech>
2021-11-27 22:00:07 -0800

    NOTE: only commiting .env because of capstone requirements.

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
+const { auth, requiresAuth } = require("express-openid-connect");
+
+// Routers
 const dashboardRouter = require("./routes/dashboard");
-const authRouter = require("./routes/auth.js");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true })); // Access req.body from any middleware
 app.use(express.json()); // Parse JSON in req.body from any middleware
 
-app.get("/", (_req, res) => {
-    res.redirect("/signup");
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: "http://localhost:5001",
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUERBASEURL,
+    secret: process.env.SECRET,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+// creates the session
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", requiresAuth(), (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
-app.use("/dashboard", dashboardRouter);
-app.use("/auth", authRouter);
+app.post("/callback", (_req, res) => {
+    res.redirect("/");
+});
+
+app.use("/dashboard", requiresAuth(), dashboardRouter);
 
 app.listen(5001);
